5441d9906cbd8f7c0ad46c5ad8658884bd8fa516
juhaodong <juhaodong@gmail.com>
2021-05-23 18:19:21 +0200

    初步完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer permanent app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            生成器生成器
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            一个生成各种生成器的生成器
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+          subheader
+          nav
+      >
+        <v-subheader>生成生成器</v-subheader>
+        <v-divider></v-divider>
+        <v-list-item
+            link
+            to="/create"
+        >
+          <v-list-item-content>
+            <v-list-item-title>生成</v-list-item-title>
+            <v-list-item-subtitle>重新<b>生成</b>一个生成器</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item
+            link
+        >
+          <v-list-item-content>
+            <v-list-item-title>导入</v-list-item-title>
+            <v-list-item-subtitle>导入现有的生成器</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider>s</v-divider>
+        <v-subheader>已经生成的生成器</v-subheader>
+        <v-divider></v-divider>
+        <v-list-item
+            link
+        >
+          <v-list-item-content>
+            <v-list-item-title>彩虹屁生成器</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item
+            link
+        >
+          <v-list-item-content>
+            <v-list-item-title>彩虹屁生成器</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <router-view/>
+    </v-main>
+
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
